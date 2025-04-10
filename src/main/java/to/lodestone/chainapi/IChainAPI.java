1313cc603dbb8890736df251857a585d1b4d1dce
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-04-09 18:47:29 -0700

    feat: Update Chain API to v1.0.12

--- src/main/java/to/lodestone/chainapi/IChainAPI.java
+++ src/main/java/to/lodestone/chainapi/IChainAPI.java
@@ -1,12 +1,9 @@
 package to.lodestone.chainapi;
 
-import org.bukkit.entity.LivingEntity;
-
 public interface IChainAPI {
 
-    void chain(LivingEntity victimOne, LivingEntity victimTwo);
-    void unchain(LivingEntity livingEntity);
-    boolean isChained(LivingEntity livingEntity);
-    boolean isChainedWith(LivingEntity victimOne, LivingEntity victimTwo);
+    IChainManager getChainManager();
+
+    void setMaxDistance(int maxDistance);
 
 }
---
+++ src/main/java/to/lodestone/chainapi/IChainManager.java
@@ -0,0 +1,15 @@
+package to.lodestone.chainapi;
+
+import org.bukkit.entity.LivingEntity;
+
+public interface IChainManager {
+
+    void chain(LivingEntity victimOne, LivingEntity victimTwo);
+
+    void unchain(LivingEntity livingEntity);
+
+    boolean isChained(LivingEntity livingEntity);
+
+    boolean isChainedWith(LivingEntity victimOne, LivingEntity victimTwo);
+
+}
